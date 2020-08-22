c6cdab9c8eb133d17b77a0999ba781f61f3c4a3a
RandyWardhana <randywardhana37@gmail.com>
2020-08-22 13:38:23 +0700

    Implement Widget Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:firstproject/navigateClass/first_page.dart';
-import 'package:firstproject/navigateClass/second_page.dart';
-import 'package:firstproject/navigateClass/third_page.dart';
+// StateLess and StateFull
+import 'stateLessAndStateFull/body.dart';
+
+// Navigation Class
+import 'navigateClass/body.dart';
+
+// Widget Container
+import 'widgetContainer/body.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,101 +21,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PageUtama(),
-      // home: PageStateFull(),
+      home: PageWidgetContainer(), // Widget Container
+      // home: PageUtama(), // Navigate Class
+      // home: PageStateFull(), // StateLess and stateFull
       debugShowCheckedModeBanner: false
     );
   }
 }
-
-class PageUtama extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: new Center(
-        child: Column(
-          children: <Widget>[
-            new MaterialButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => PagePertama()));
-              },
-              child: Text('Page Pertama'),
-              color: Colors.green,
-              textColor: Colors.white
-            ),
-            new MaterialButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => PageKedua()));
-              },
-              child: Text('Page Pertama'),
-              color: Colors.green,
-              textColor: Colors.white
-            ),
-            new MaterialButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => PageKetiga()));
-              },
-              child: Text('Page Pertama'),
-              color: Colors.green,
-              textColor: Colors.white
-            )
-          ],
-        ),
-      )
-    );
-  }
-}
-
-// // Stateless & StateFull
-// class PageStateless extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: new Container(
-//         color: Colors.blueAccent,
-//         child: new Container(
-//           color: Colors.lightGreen,
-//           margin: const EdgeInsets.all(50.0)
-//         )
-//       )
-//     );
-//   }
-// }
-
-// class PageStateFull extends StatefulWidget {
-//   @override
-//   _PageStateFullState createState() => _PageStateFullState();
-// }
-
-// class _PageStateFullState extends State<PageStateFull> {
-
-//   String text;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     text = 'Click this button!';
-//   }
-
-//   void methodChange() {
-//     setState(() {
-//       text = 'Hello 世界!';
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: new Center(
-//         child: new MaterialButton(
-//           child: Text(text),
-//           color: Colors.green,
-//           textColor: Colors.white,
-//           onPressed: () {
-//             methodChange();
-//           }
-//         ),
-//       ),
-//     );
-//   }
-// }
