d6fc460b8498d067d55ccd4203cb209b71f00c63
RandyWardhana <randywardhana37@gmail.com>
2020-08-22 13:59:52 +0700

    Rename WidgetContainer to ContainerWidget

--- lib/containerWidget/body.dart
+++ lib/containerWidget/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PageWidgetContainer extends StatelessWidget {
+class PageContainerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
