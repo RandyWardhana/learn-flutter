32a9eed7782e1166cf990449baf3c89c22d73b3a
RandyWardhana <randywardhana37@gmail.com>
2020-08-22 16:27:51 +0700

    Create file for PageGridView Class

---
+++ lib/gridView/body.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class PageGridView extends StatefulWidget {
+  @override
+  _PageGridViewState createState() => _PageGridViewState();
+}
+
+class _PageGridViewState extends State<PageGridView> {
+  List<int> itemGrid = new List();
+
+  @override
+  void initState() {
+    super.initState();
+
+    for(int i = 0; i < 30; i++) {
+      itemGrid.add(i);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new GridView.builder(
+        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+        itemBuilder: (BuildContext context, int index) {
+          return new Card(
+            color: Colors.blueAccent,
+            child: new Padding(padding: const EdgeInsets.all(8),),
+          );
+        },
+      ),
+    );
+  }
+}
