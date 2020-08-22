4f69001cf5d9b8beb677f7321cf675ae183803f0
RandyWardhana <randywardhana37@gmail.com>
2020-08-22 13:39:52 +0700

    Create file for widgetContainer Class

---
+++ lib/widgetContainer/body.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PageWidgetContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: new Text('Widget Container'),
+        width: 300.0,
+        height: 500.0,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(20),
+        foregroundDecoration: new BoxDecoration(
+          color: Colors.blueAccent
+        ),
+      )
+    );
+  }
+}
