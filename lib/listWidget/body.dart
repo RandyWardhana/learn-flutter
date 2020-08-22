a7a27f1bddbce8afd4d5f4207b717298979883ca
RandyWardhana <randywardhana37@gmail.com>
2020-08-22 14:16:23 +0700

    Create List Widget Array

--- lib/listWidget/body.dart
+++ lib/listWidget/body.dart
@@ -31,3 +31,36 @@ class PageList extends StatelessWidget {
     );
   }
 }
+
+class PageListWidget extends StatefulWidget {
+  @override
+  _PageListWidgetState createState() => _PageListWidgetState();
+}
+
+class _PageListWidgetState extends State<PageListWidget> {
+  List<int> item = new List();
+
+  @override
+  void initState() {
+    super.initState();
+
+    for(int i = 0; i < 25; i++) {
+      item.add(i);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new ListView.builder(
+        itemCount: item.length,
+        itemBuilder: (BuildContext context, int index) {
+          return new ListTile(
+            title: new Text("Item Number: ${index + 1}"),
+            trailing: new Icon(Icons.print),
+          );
+        },
+      ),
+    );
+  }
+}
